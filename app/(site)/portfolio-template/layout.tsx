b06b4b3748317ed0f56271621ee5202609d72ef0
EdSpurrier <ed@edspurrier.com>
2023-09-09 13:24:16 +1000

    Setting up app using base from portfolio-template

--- app/(site)/portfolio-template/layout.tsx
+++ app/(site)/portfolio-template/layout.tsx
@@ -1,7 +1,8 @@
-import "../globals.css";
+import "../../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import SideMenu from "./components/global/SideMenu";
+import Navbar from "./components/global/Navbar";
+import Footer from "./components/global/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-zinc-900 text-white`}>
-        <SideMenu />
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/(site)/portfolio-template/page.tsx
+++ app/(site)/portfolio-template/page.tsx
@@ -1,5 +1,7 @@
 import { getProfile } from "@/sanity/sanity.query";
 import type { ProfileType } from "@/types";
+import HeroSvg from "./icons/HeroSvg";
+import Job from "./components/Job";
 
 export default async function Home() {
   const profile: ProfileType[] = await getProfile();
@@ -34,7 +36,9 @@ export default async function Home() {
               </ul>
             </div>
           ))}
+        <HeroSvg />
       </section>
+      <Job />
     </main>
   );
 }
